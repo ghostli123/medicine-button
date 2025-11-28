6586b32260c0f018f3c7ae025d6a2f3bb284281d
Yang Zhang <zhan3248@umn.edu>
2025-11-27 23:36:23 -0800

    Initial Commit

---
+++ TakingMedicine/TakingMedicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakingMedicineApp.swift
+//  TakingMedicine
+//
+//  Created by Yang Zhang on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TakingMedicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TakingMedicineTests/TakingMedicineTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakingMedicineTests.swift
+//  TakingMedicineTests
+//
+//  Created by Yang Zhang on 11/27/25.
+//
+
+import Testing
+@testable import TakingMedicine
+
+struct TakingMedicineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
